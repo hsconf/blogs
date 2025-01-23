d6c169c45bd909d0b94b2f53836dd710ed5bef39
hsconf <hsplay74@gmail.com>
2025-01-23 13:55:25 +0600

    Update index.js

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -14,7 +14,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const user_1 = __importDefault(require("./routes/user"));
-const post_1 = __importDefault(require("./routes/post"));
+const post_1 = __importDefault(require("./routes/poste"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
